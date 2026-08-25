211d662806e0f03f4b5a7a8d8bbfcc508d94bd07
betel_espoir10 <espoirgonbeloum@gmail.com>
2026-08-25 14:36:44 +0200

    Ajout code dans fichier exercie1

---
+++ section_1/exercice_01.py
@@ -0,0 +1,72 @@
+#La fonction print()
+
+print("Hello, world !")
+
+# backslash (\) has a very special meaning when used inside strings ‒ this is called the escape character.
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+
+#using keyword function : a keyword argument consists of three elements: a keyword identifying the argument (end here); an equal sign (=); and a value assigned to that argument;
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+#Keyword separator and end
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+print("                      ")
+#formating the output
+print("************ original version **************:")
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+print("********* Solution ******************:")
+###################
+print("with fewer 'print()' invocations:")
+###################
+print("    *\n   * *\n  *   *\n *     *\n***   ***")
+print("  *   *\n  *   *\n  *****")
+###################
+print("higher:")
+###################
+print("        *")
+print("       * *")
+print("      *   *")
+print("     *     *")
+print("    *       *")
+print("   *         *")
+print("  *           *")
+print(" *             *")
+print("******     ******")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *******")
+###################
+print("doubled the output:")
+###################
+print("        *        "*2)
+print("       * *       "*2)
+print("      *   *      "*2)
+print("     *     *     "*2)
+print("    *       *    "*2)
+print("   *         *   "*2)
+print("  *           *  "*2)
+print(" *             * "*2)
+print("******     ****** " *2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *******     "*2)
